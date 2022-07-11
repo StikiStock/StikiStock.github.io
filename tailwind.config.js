e8c429a0bd8e51679f2636b4df0828e1c81a307c
Stiki Stock <87359144+StikiStock@users.noreply.github.com>
2022-07-11 11:54:16 +0430

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      keyframes: {},
+      keyframes: {
+        blur: {
+            '0%': {
+              backdrop: 'blur(24px)'
+            },
+            '100%': {
+              backdrop: 'blur(0px)'
+            },
+        }
+    },
+    animation: {
+        blur_out: 'blur 500ms ease-out forwards',
+    }
     },
   },
   darkMode: 'media',
